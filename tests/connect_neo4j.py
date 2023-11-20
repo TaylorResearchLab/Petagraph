bf7f821d2b6421bc392cfc3a5acd494fb07bc76a
ben stear <bjs385@drexel.edu>
2023-11-19 19:26:02 -0500

    Create connect_neo4j.py

---
+++ tests/connect_neo4j.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from neo4j import GraphDatabase, basic_auth
+
+uri='bolt://localhost:7687'
+user='neo4j'
+password='neo4j2020'
+
+driver = GraphDatabase.driver(uri, auth=(user, password))
