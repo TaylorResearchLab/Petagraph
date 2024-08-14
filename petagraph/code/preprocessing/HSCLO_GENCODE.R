0a945cf3d70c96387e7845ee201ade6e5add61b8
tma1 <107221080+tma1@users.noreply.github.com>
2024-08-14 14:26:14 -0400

    Update HSCLO_GENCODE.R

--- petagraph/code/preprocessing/HSCLO_GENCODE.R
+++ petagraph/code/preprocessing/HSCLO_GENCODE.R
@@ -1,1 +1,3 @@
-for (i in 1:295559) {l <- floor(as.numeric(G[i,2])/1000)*1000; if (G[i,2]>l) {L[i,3] = paste("HSCLO"," ",G[i,6],".",l+1,"-",l+1000,sep = "")} else {L[i,3] = paste("HSCLO"," ",G[i,6],".",l-999,"-",l,sep = "")}}
+#THis scripts creates GENCODE mapping of transcript and genes found in Petgraph to HSCLO nodes at 1kbp resolution
+for (i in 1:295559) {l <- floor(as.numeric(G[i,2])/1000)*1000; 
+if (G[i,2]>l) {L[i,3] = paste("HSCLO"," ",G[i,6],".",l+1,"-",l+1000,sep = "")} else {L[i,3] = paste("HSCLO"," ",G[i,6],".",l-999,"-",l,sep = "")}}
