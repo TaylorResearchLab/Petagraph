0a6ce743f82bbfb778720c85069b021c094f16a7
ben stear <bjs385@drexel.edu>
2025-12-11 08:15:25 -0500

    Refactor password handling in reset_password.py
    Removed hardcoded password and updated password query.

--- tests/reset_password.py
+++ tests/reset_password.py
@@ -6,12 +6,10 @@ from neo4j import GraphDatabase, basic_auth
 
 uri='bolt://localhost:7687'
 user='neo4j'
-#password='neo4j2020'
-password='neo4j'
 #password=args.NEO4J_PASSWORD
 
 driver = GraphDatabase.driver(uri, auth=(user, password))
 
-pw_query='''ALTER CURRENT USER SET PASSWORD FROM "neo4j" TO "neo4j2020"'''
+pw_query=''' '''
 with driver.session(default_access_mode=neo4j.WRITE_ACCESS) as session:
         result = session.run(pw_query)
